baf7046c19d248f60ddddbc6fbc65c3f8faa2503
alexandre <sousa.akira@gmail.com>
2014-12-12 23:01:50 -0300

    atuali

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexandre
+ * Date: 12/12/14
+ * Time: 22:00
+ */
+
+$host       = '127.0.0.1';
+$user       = 'root';
+$pass       = '455ttte';
+$base       = 'biit';
+
+$db = new mysqli($host,$user,$pass,$base);
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once ('includes/config.php');
 // não existe variável via url , a ligua é pt
 if(!isset($_GET["lang"])){
     $lingua = "pt_br";
@@ -9,27 +10,30 @@ if(!isset($_GET["lang"])){
 
 // impede que sejam carregados ficheiros diferentes
 if($lingua=="pt_br" || $lingua=="en"){
+
 // utiliza o valor da variável  $lingua para carregar o ficheiro
     include( 'i18n/'.$lingua.".php");
 }
 // cria os links para selecção das línguas
-echo "<a href=\"". basename(__FILE__)."?lang=pt_br\" target=\"_self\">
-Português </a>";
-echo "&nbsp;&nbsp;";
-echo "<a href=\"". basename(__FILE__)."?lang=en\" target=\"_self\">
-English </a>";
 
-//imprime o texto de acordo com a liguagem escolhida
-echo "<br><hr><b>";
-echo HELLO;
-echo "</b>";
 
+/**
+ * envio da busca
+ */
+
+$busca = $_POST['busca'];
+$idf = "%$busca%";
+
+if(isset($busca)) {
+    $qr_busca = $db->query("SELECT * FROM id_sites WHERE `desciption` LIKE CONVERT(_utf8 '$idf' USING latin1)");
+}
 ?>
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head lang="pt-br">
   <meta charset="UTF-8">
   <title>Buscador Web</title>
+    <script type="text/javascript" src="js/jquery-1.7.2.js"></script>
     <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
     <link href="css/styler.css" rel="stylesheet">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -37,27 +41,65 @@ echo "</b>";
 </head>
 <body>
 
-<div class="topo">Teste | Teste | Teste | Teste</div>
+<ul class="nav ">
 
-<div id="div">
-<form method="post"action="" enctype="multipart/form-data" name="busca" id="busca">
 
-Buscador: Biit
+    <a href="<?php echo  basename(__FILE__)."?lang=pt_br" ?>"><span class="label label-success">PT-BR</span>
+    <a href="<?php echo  basename(__FILE__)."?lang=en" ?>"><span class="label label-primary">EN</span></a>
+    <span class="label label-default">????</span>
+    <span class="label label-info">????</span>
+    <span class="label label-warning">????</span>
 
-    <div class="row">
-         <div class="col-lg-6">
-            <div class="input-group">
-                <input type="text" name="io" id="io" class="form-control">
-      <span class="input-group-btn">
-        <input class="btn btn-default" value="<?php echo GO ?>" type="submit">
-      </span>
-            </div><!-- /input-group -->
-        </div><!-- /.col-lg-6 -->
-    </div><!-- /.row -->
 
+</ul>
+
+<hr>
+
+<b>
+
+<?php
+$qt_busca = $qr_busca->num_rows;
+if($qt_busca > 0){
+
+  }else{ ?>
+        <div id="div">
+    <?php
+    }
+    ?>
+<form role="form" method="post"action="" enctype="multipart/form-data" name="busca" id="busca">
+    <div class="form-group">
+        <label for="searchBar">Buscador Sintag</label>
+        <div class="input-group" style="width: 400px;"><!--Estava faltando essa div-->
+            <input type="text" class="form-control" name="busca" id="searchBar" placeholder="Procurar por:" />
+            <span class="input-group-btn"><!--Estava faltando esse span-->
+                <button style="height: 34px;" type="submit" class="btn btn-info" value="Procurar">
+                    <span class="glyphicon glyphicon-search"></span>
+                </button>
+            </span>
+        </div>
+    </div>
 </form>
-    Ola
-</div>
+    <?php if($qt_busca > 0){ ?>
+    <hr>
+
+        <?php
+        while($row = $qr_busca->fetch_array()){
+        ?>
+
+           <b><a><a href="http://<?php echo $row[2];?> " target="_blank" style="font-size: 12px; color: #2a6496;"><?php echo $row[1].'<br>'; ?></a> </b>
+            <i><font size="2"> <?php echo utf8_encode($row[2]).'<br>'; ?></font></i>
+           <b><a><a href="http://<?php echo $row[2];?>" target="_blank"  style="font-size: 12px; color: green;"><?php echo $row[2].'<br>'; ?></a> </b>
+
+       <?php
+        }
+        ?>
+
+        <?php }else{
+            ?>
+        </div>
+        <?
+        }
+        ?>
     <?php ?>
 </body>
 </html>
